5403733132811c4c8a93a138683128a61695918f
Philipp Deppenwiese <zaolin@das-labor.org>
2018-05-30 16:41:24 +0200

    Fix comments in review

--- pkg/rng/entropy.go
+++ pkg/rng/entropy.go
@@ -2,52 +2,71 @@ package rng
 
 import (
 	"io/ioutil"
+	"log"
 	"os"
 	"strconv"
 	"strings"
 	"time"
 )
 
+// The concept
+//
+// Most systems in real world application do not provide enough entropy at boot
+// time. Therefore we will seed /dev/random with /dev/hwrng if a HW random
+// number generator is available. Entropy is important for cryptographic
+// protocols running in network stacks. Also disk encryption can be a problem
+// if bad or no entropy is available. It can either block provisioning or makes
+// a symmetric key easy to re-calculate.
+
 var (
-	trngList map[string]int
+	// HwRandomCurrentFile shows/sets the current
+	// HW random number generator
+	HwRandomCurrentFile = "/sys/class/misc/hw_random/rng_current"
+	// HwRandomAvailableFile shows the current available
+	// HW random number generator
+	HwRandomAvailableFile = "/sys/class/misc/hw_random/rng_available"
+	// RandomPoolSizeFile returns the entropy poolsize
+	RandomPoolSizeFile = "/proc/sys/kernel/random/poolsize"
+	// RandomEntropyAvailableFile shows how much of the entropy poolsize is used
+	RandomEntropyAvailableFile = "/proc/sys/kernel/random/entropy_avail"
+	// EntropyFeedTime sets the loop time for seeding /dev/random by /dev/hwrng
+	// in seconds
+	EntropyFeedTime time.Duration = 10
+	// RandomDevice is the linux random device
+	RandomDevice = "/dev/random"
+	// HwRandomDevice is the linux hw random device
+	HwRandomDevice = "/dev/hwrng"
 )
 
-const (
-	hwRandomDir                string        = "/sys/class/misc/hw_random"
-	hwRandomCurrentFile        string        = "/sys/class/misc/hw_random/rng_current"
-	hwRandomAvailableFile      string        = "/sys/class/misc/hw_random/rng_available"
-	randomPoolSizeFile         string        = "/proc/sys/kernel/random/poolsize"
-	randomEntropyAvailableFile string        = "/proc/sys/kernel/random/entropy_avail"
-	entropyFeedTime            time.Duration = 10
-	randomDevice               string        = "/dev/random"
-	hwRandomDevice             string        = "/dev/hwrng"
-)
-
-func init() {
-	trngList = make(map[string]int)
-
-	// Priorize the HW RNG 0 is the highest
-	trngList["tpm-rng"] = 0
-	trngList["intel-rng"] = 0
-	trngList["amd-rng"] = 0
-	trngList["timeriomem-rng"] = 3
+// Can be extended but keep in mind to priorize
+// more secure random sources like hw random over
+// timer, jitter based mechanisms. Zero is the highest
+// priority.
+// <rng-name> : <priority>
+var trngList = map[string]int{
+	"tpm-rng":        0,
+	"intel-rng":      1,
+	"amd-rng":        1,
+	"timeriomem-rng": 2,
 }
 
+// Searches for available True Random Number Generator
+// inside the kernel api and sets the most secure on if
+// available which seeds /dev/hwrng
 func setAvailableTRNG() (bool, error) {
-	var currentRNG string
-	var availableRNGs []string
-	var rngs []string
-	if _, err := os.Stat(hwRandomDir); os.IsNotExist(err) {
-		return false, err
-	}
+	var (
+		currentRNG    string
+		availableRNGs []string
+		rngs          []string
+	)
 
-	currentFileData, err := ioutil.ReadFile(hwRandomCurrentFile)
+	currentFileData, err := ioutil.ReadFile(HwRandomCurrentFile)
 	if err != nil {
 		return false, err
 	}
 	currentRNG = string(currentFileData)
 
-	availableFileData, err := ioutil.ReadFile(hwRandomAvailableFile)
+	availableFileData, err := ioutil.ReadFile(HwRandomAvailableFile)
 	if err != nil {
 		return false, err
 	}
@@ -71,69 +90,79 @@ func setAvailableTRNG() (bool, error) {
 		return false, nil
 	}
 
-	ioutil.WriteFile(hwRandomCurrentFile, []byte(rngs[0]), 0644)
+	ioutil.WriteFile(HwRandomCurrentFile, []byte(rngs[0]), 0644)
 
 	return true, nil
 }
 
-// UpdateLinuxRandomness should be executed
-// as goroutine for constantly filling hw_random
-// into the Linux random device. Function will auto
-// block in case of no random in hw_rng.
-func UpdateLinuxRandomness() error {
+// UpdateLinuxRandomness seeds random data from
+// /dev/hwrng into /dev/random based on a timer and
+// the entropy pool size
+// Usage:
+// go UpdateLinuxRandomness()
+func UpdateLinuxRandomness() {
 	good, err := setAvailableTRNG()
 	if !good {
-		return err
+		log.Fatalf("Can't set available TRNG: %s", err.Error())
+		return
 	}
 
-	randomPoolSizeData, err := ioutil.ReadFile(randomPoolSizeFile)
+	randomPoolSizeData, err := ioutil.ReadFile(RandomPoolSizeFile)
 	if err != nil {
-		return err
+		log.Fatalf("Can't random poolsize data: %s", err.Error())
+		return
 	}
 
 	formatted := strings.TrimSuffix(string(randomPoolSizeData), "\n")
 	randomPoolSize, err := strconv.ParseUint(formatted, 10, 32)
 	if err != nil {
-		return err
+		log.Fatalf("Can't parse random poolsize: %s", err.Error())
+		return
 	}
 
-	hwRng, err := os.OpenFile(hwRandomDevice, os.O_RDONLY, os.ModeDevice)
+	hwRng, err := os.OpenFile(HwRandomDevice, os.O_RDONLY, os.ModeDevice)
 	if err != nil {
-		return err
+		log.Fatalf("Can't open /dev/hwrng: %s", err.Error())
+		return
 	}
 
 	defer hwRng.Close()
 
-	rng, err := os.OpenFile(randomDevice, os.O_APPEND|os.O_WRONLY, os.ModeDevice)
+	rng, err := os.OpenFile(RandomDevice, os.O_APPEND|os.O_WRONLY, os.ModeDevice)
 	if err != nil {
-		return err
+		log.Fatalf("Can't open /dev/random: %s", err.Error())
+		return
 	}
 
 	defer rng.Close()
 
 	for {
-		randomEntropyAvailableData, err := ioutil.ReadFile(randomEntropyAvailableFile)
+		randomEntropyAvailableData, err := ioutil.ReadFile(RandomEntropyAvailableFile)
 		if err != nil {
-			return err
+			log.Fatalf("Can't read available entropy: %s", err.Error())
+			return
 		}
 
 		formatted := strings.TrimSuffix(string(randomEntropyAvailableData), "\n")
 		randomEntropyAvailable, err := strconv.ParseUint(formatted, 10, 32)
 		if err != nil {
-			return err
+			log.Fatalf("Can't parse available entropy: %s", err.Error())
+			return
 		}
 
 		randomBytesNeeded := randomPoolSize - randomEntropyAvailable
 		if randomBytesNeeded > 0 {
 			var random = make([]byte, randomBytesNeeded)
 			if _, err = hwRng.Read(random); err != nil {
-				return err
+				log.Fatalf("Can't read random from /dev/hwrng: %s", err.Error())
+				return
 			}
 			if _, err = rng.Write(random); err != nil {
-				return err
+				log.Fatalf("Can't write random to /dev/random: %s", err.Error())
+				return
 			}
 		}
 
-		time.Sleep(entropyFeedTime * time.Second)
+		time.Sleep(EntropyFeedTime * time.Second)
 	}
 }
