eefc3385a884c74044f7370f6475df76595348e2
Jens Drenhaus <jens.drenhaus@9elements.com>
2019-04-26 14:45:14 +0200

    add netvars.json to partition
        netvars.json contains:
        * host IP
        * netmask
        * default gateway
        * DNS Server
        * host priv/pub keys
        * bootstrap URL
        * signature pub key
    
        Try to mount every detected blockdevice. Then find and parse netvars.json
        and set up the network interface with the given data.
    
    Signed-off-by: Jens Drenhaus <jens.drenhaus@9elements.com>

--- ipboot/main.go
+++ ipboot/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"path"
+	"syscall"
+
+	"github.com/systemboot/systemboot/pkg/storage"
 )
+
+// TODO
+// implement booter interface
+// create bootconfig
+// signature verification
 
 var (
 	dryRun  = flag.Bool("dryrun", false, "Do everything except booting the loaded kernel")
@@ -16,10 +24,11 @@ var (
 )
 
 const (
-	ip      = "10.0.2.15/24"
-	gateway = "10.0.2.2/24"
-	eth     = "eth0"
-	url     = "http://mullvad.9esec.io/vmlinuz-fedora"
+	ip          = "10.0.2.15/24"
+	gateway     = "10.0.2.2/24"
+	eth         = "eth0"
+	url         = "http://mullvad.9esec.io/vmlinuz-fedora"
+	netVarsPath = "netvars.json"
 )
 
 var banner = `
@@ -52,21 +61,81 @@ func main() {
 	}
 	log.Print(banner)
 
-	//get network variables
-	file, err := os.Open("/root/netvars.json")
+	// get block devices
+	devices, err := storage.GetBlockStats()
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	defer file.Close()
-	data, _ := ioutil.ReadAll(file)
-	fmt.Println(string(data))
+	// print partition info
+	if *doDebug {
+		for _, dev := range devices {
+			log.Printf("Device: %+v", dev)
+		}
+	}
+
+	// get a list of supported file systems for real devices (i.e. skip nodev)
+	debug("Getting list of supported filesystems")
+	filesystems, err := storage.GetSupportedFilesystems()
+	if err != nil {
+		log.Fatal(err)
+	}
+	debug("Supported file systems: %v", filesystems)
+
+	var mounted []storage.Mountpoint
+	// try mounting all the available devices, with all the supported file
+	// systems
+	debug("trying to mount all the available block devices with all the supported file system types")
+	mounted = make([]storage.Mountpoint, 0)
+	for _, dev := range devices {
+		devname := path.Join("/dev", dev.Name)
+		mountpath := path.Join("/mnt", dev.Name)
+		if mountpoint, err := storage.Mount(devname, mountpath, filesystems); err != nil {
+			debug("Failed to mount %s on %s: %v", devname, mountpath, err)
+		} else {
+			mounted = append(mounted, *mountpoint)
+		}
+	}
+	log.Printf("mounted: %+v", mounted)
+	defer func() {
+		// clean up
+		for _, mountpoint := range mounted {
+			syscall.Unmount(mountpoint.Path, syscall.MNT_DETACH)
+		}
+	}()
+
+	// search for a netvars.json
+	var data []byte
+	for _, mountpoint := range mounted {
+		path := path.Join(mountpoint.Path, netVarsPath)
+		log.Printf("Trying to read %s", path)
+		data, err = ioutil.ReadFile(path)
+		if err == nil {
+			break
+		}
+		log.Printf("cannot open %s: %v", path, err)
+	}
+
+	log.Printf("Parse network variables")
 	vars := netVars{}
 	json.Unmarshal(data, &vars)
-	fmt.Printf("Parsed network variables: %v\n", vars)
+	// FIXME : error handling
+	// print network variables
+	if *doDebug {
+		log.Print("HostIP: " + vars.HostIP)
+		log.Print("HostNetmask: " + vars.HostNetmask)
+		log.Print("DefaultGateway: " + vars.DefaultGateway)
+		log.Print("DNSServer: " + vars.DNSServer)
+
+		log.Print("HostPrivKey: " + vars.HostPrivKey)
+		log.Print("HostPubKey: " + vars.HostPupKey)
+
+		log.Print("BootstrapURL: " + vars.BootstrapURL)
+		log.Print("SignaturePupKey: " + vars.SignaturePubKey)
+	}
 
 	//setup ip
-	cmd := exec.Command("ip", "addr", "add", ip, "dev", eth)
+	log.Print("Setup network configuration with IP: " + vars.HostIP)
+	cmd := exec.Command("ip", "addr", "add", vars.HostIP, "dev", eth)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
@@ -78,14 +147,30 @@ func main() {
 	if err := cmd.Run(); err != nil {
 		log.Printf("Error executing %v: %v", cmd, err)
 	}
-	cmd = exec.Command("ip", "route", "add", "default", "via", gateway, "dev", eth)
+	cmd = exec.Command("ip", "route", "add", "default", "via", vars.DefaultGateway, "dev", eth)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
 		log.Printf("Error executing %v: %v", cmd, err)
 	}
 
+	if *doDebug {
+		cmd = exec.Command("ip", "addr")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			log.Printf("Error executing %v: %v", cmd, err)
+		}
+		cmd = exec.Command("ip", "route")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			log.Printf("Error executing %v: %v", cmd, err)
+		}
+	}
+
 	//get kernel
+	log.Print("Get kernel from " + url)
 	cmd = exec.Command("wget", "-O", "ipkernel", url)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -93,9 +178,8 @@ func main() {
 		log.Printf("Error executing %v: %v", cmd, err)
 	}
 
-	// TODOcreate boot config
-
 	//boot
+	log.Print("Load kernel")
 	cmd = exec.Command("kexec", "-l", "ipkernel", "-c", "console=ttyS0,115200")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -104,10 +188,11 @@ func main() {
 	}
 
 	if *dryRun {
-		debug("Dry-run mode: will not boot")
+		debug("Dryrun mode: will not boot")
 		return
 	}
 
+	log.Print("Do kexec")
 	cmd = exec.Command("kexec", "-e")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
